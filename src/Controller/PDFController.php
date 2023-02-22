e74e5c8868013935ad9d9d138c211c0498c226b2
Oussama-Naffeti <80197746+Oussama-Naffeti@users.noreply.github.com>
2023-02-22 21:05:42 +0100

    Commit 1.2

---
+++ src/Controller/PDFController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use ContainerJA2Xfg6\getPlannificationControllerService;
+use Dompdf\Dompdf;
+use App\Entity\Plannification;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PDFController extends AbstractController
+{
+    #[Route('/generate-pdf/{id}', name: 'app_generate_pdf')]
+    public function generatePdf($id)
+    {
+        // Récupérer la plannification à afficher en PDF
+        $plannification = $this->getDoctrine()->getRepository(Plannification::class)->find($id);
+
+        // Créer une instance de Dompdf
+        $dompdf = new Dompdf();
+
+        // Générer le HTML à partir d'un template Twig
+        $html = $this->renderView('pdf/plannification.html.twig', [
+            'plannification' => $plannification
+        ]);
+
+        // Charger le HTML dans Dompdf
+        $dompdf->loadHtml($html);
+
+        // Rendre le PDF
+        $dompdf->render();
+
+        // Renvoyer le PDF au navigateur
+        return new Response($dompdf->output(), 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="plannification.pdf"'
+        ]);
+    }
+}
--- src/Entity/Salle.php
+++ src/Entity/Salle.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use http\Message;
 use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping\UniqueEntity;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SalleRepository::class)]
