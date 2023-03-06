8a4329a6c0987f342fd374757f887a47707ca9fb
khaled baoueb <mouhamedkhaled.baoueb@esprit.tn>
2023-03-06 01:22:13 +0100

    *

--- src/Entity/Categoriesvehicules.php
+++ src/Entity/Categoriesvehicules.php
@@ -8,7 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Serializer\Annotation\Groups;
-//
+/////
 #[ORM\Entity(repositoryClass: CategoriesvehiculesRepository::class)]
 class Categoriesvehicules
 {
